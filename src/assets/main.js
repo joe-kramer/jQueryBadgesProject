3d5132dcda53b0efb379c4dc6092d66daee56de5
joe-kramer <joekramer@Joes-MacBook-Pro.local>
2017-04-05 13:44:36 -0700

    Update two

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
+  $.ajax({
+      url: 'https://www.codeschool.com/users/2728572.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        console.log('response', response.courses.completed);
+        addCourses(response.courses.completed);
+      }
+    });
 
-  // your code will go here
+    function addCourses(courses) {
 
+      var $badges = $('#badges');
+
+      for(var i = 0; i < courses.length; i++){
+        $(<div)
+      }
+    }
 });
